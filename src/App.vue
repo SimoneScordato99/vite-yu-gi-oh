e4488c05af857fe29c5cccc85f38794da5d26902
SimoneScordato99 <scordato.simone@yahoo.com>
2023-05-05 17:46:14 +0200

    start

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <script >
+  import AppHeader from './components/AppHeader.vue'
+  import AppMain from './components/AppMain.vue'
+  // import axios from 'axios'
+  // import {store} from './store'
 
+  export default {
+    name: 'App',
+    components:{
+      AppHeader,
+      AppMain
+    },
+    // data(){
+    //   store
+    // },
+    // created(){
+    //   this.api()
+    // },
+    // methods:{
+    //   api(){
+    //     axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=1')
+    //       .then((elemento) =>{
+    //         const Apii = elemento.data.data[0].name
+    //         this.store.carte = Apii
+    //         console.log(this.store.carte)
+    //       } )
+    //   }
+    // }
+  }
 </script>
 
 <template>
-  <h1>Ciao mondo</h1>
+    <AppHeader/>
+    <AppMain/>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script >
+
+  export default {
+    name: 'AppHeader'
+  }
+</script>
+
+<template>
+    <head>
+        <h1>ofbnvgoqa</h1>
+    </head>
+</template>
+
+<style lang="scss" scoped>
+    head{
+        height: 80px;
+        width: 100%;
+        background-color: orange;
+    }
+</style>
